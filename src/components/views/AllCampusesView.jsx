6054a85b80313ab80719c2f745f4d8db1a85329d
RyanGlem <ryanglem@hotmail.com>
2021-01-24 13:39:11 -0500

    Changed campus view

--- src/components/views/AllCampusesView.jsx
+++ src/components/views/AllCampusesView.jsx
@@ -4,29 +4,35 @@ import { deleteCampusThunk } from '../../store/utilities/CampusThunk';
 const AllCampusesView = (props) => {
 	return (
 		<div className="All-Campuses">
-			<ul>
 				{props.allCampuses !== undefined ? (
 					props.allCampuses.map((element, index) => {
 						return (
+							<table>
 							<li key={element.name + element.id}>
 								<div className="Campus">
+									<tr>
 									<img className="campusImg" src={element.imageUrl} alt="campusPicture" />
 									<button className={element.id} onclick={deleteCampusThunk(element.id)}>
 										X
 									</button>{' '}
+									</tr>
 									<br />
+								<tr>
+									<td>
 									Campus: {element.name} <br />
 									Address: {element.address}
 									<br />
 									<p>{element.description}</p>
-								</div>
-							</li>
+									</td>
+								</tr>
+							</div>
+						</li>
+					</table>
 						);
 					})
 				) : (
 					<li>No campuses found!</li>
 				)}
-			</ul>
 		</div>
 	);
 };
