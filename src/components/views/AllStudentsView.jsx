174fbf682c41ff6a922c556ec5b0632eda5ff646
Raekwon Williams <Raekwill15@gmail.com>
2021-01-23 23:13:13 -0500

    Stlying and student format changed

--- src/components/views/AllStudentsView.jsx
+++ src/components/views/AllStudentsView.jsx
@@ -4,18 +4,24 @@ import {deleteStudentThunk} from '../../store/utilities/StudentThunks';
 const AllStudentsView = (props) => {
 	return (
 		<div className="student">
-			{/* An example of how each student will look can definetly be changed*/}
+			{/* An example of how each student will look can be changed*/}
 						<table>
 							<tr>
 								<td>
 							<img className="studentImg" src='https://images.generated.photos/PasmNYj9u6Xfag77ggwPkcBcqOGkBG1SZJ4B2_ybeAw/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAzNTY2MTVfMDY5/NTQ0OF8wNDI1Mzkx/LmpwZw.jpg'
 							alt="student portrait"/> 
-							<button className='a' onclick= {console.log("Hello World")}>X</button>
 							</td>
 							<td>
 							Turner, Timmy <br/>
+							</td>
+							<td>
 							Gpa: 2.7 <br/>
+							</td>
+							<td>
 							<a className="email" href='google.com'>Hello@gmail.com</a> <br/>
+							</td>
+							<td>
+							<button className='a' onClick={console.log("Hello World")}>Remove Student</button>
 							</td>
 							</tr>
 
@@ -23,13 +29,19 @@ const AllStudentsView = (props) => {
 								<td>
 							<img className="studentImg" src='https://images.generated.photos/PasmNYj9u6Xfag77ggwPkcBcqOGkBG1SZJ4B2_ybeAw/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/Z3M6Ly9nZW5lcmF0/ZWQtcGhvdG9zL3Yz/XzAzNTY2MTVfMDY5/NTQ0OF8wNDI1Mzkx/LmpwZw.jpg'
 							alt="student portrait"/> 
-							<button className='a' onclick= {console.log("Hello World")}>X</button>
-							</td>
-							<td>
-							Scott, Michael <br/>
-							Gpa: 4.0 <br/>
-							<a className="email" href='google.com'>Michael@gmail.com</a> <br/>
-							</td>
+								</td>
+								<td>
+								Scott, Michael <br/>
+								</td>
+								<td>
+								Gpa: 4.0 <br/>
+								</td>
+								<td>
+								<a className="email" href='google.com'>Michael@gmail.com</a> <br/>
+								</td>
+								<td>
+								<button className='a' onClick={console.log("Hello World")}>Remove Student</button>
+								</td>
 							</tr>
 						</table>
 
@@ -42,12 +54,18 @@ const AllStudentsView = (props) => {
 									<tr>
 										<td>
 											<img className = "studentImg" src={element.imageUrl} alt="student portrait"/> 
-											<button className={element.id} onclick= {deleteStudentThunk(element.id)}>X</button><br/>
 										</td>
 										<td>
-											{element.lastName}, {element.firstName} <br/>
+											{element.lastName}, {element.firstName}
+										</td>
+										<td>
 											Gpa: {element.gpa}
+										</td>
+										<td>
 											<a href={element.email}>{element.email}</a> <br/>
+										</td>
+										<td>
+											<button className={element.id} onclick= {deleteStudentThunk(element.id)}>Remove Student</button>
 										</td>
 									</tr>
 								);
